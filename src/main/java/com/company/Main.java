110397421226a439ef4f4abbc5e398bd1fd8b824
nikita <nikitademedyuk1@gmail.com>
2020-12-05 23:24:50 +0300

    fix

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -2,22 +2,48 @@ package com.company;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
-import static com.company.ProcessingStudents.readToListStudent;
+import static com.company.Student.*;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
         try {
             String fileName = "/Users/nikita/Documents/Hometask/Programming/Java/Hometask-9/Hometask-9/Students.txt";
-            ProcessingStudents studentList = readToListStudent(fileName, " ");
+            List<Student> studentList = readToListStudent(fileName, " ");
 
-           //for (int i = 0; i < studentList.getSizeOfStudentList(); ++i) {
-             //   System.out.print(studentList.getStudent(i));
-           //}
+            System.out.println("Do you want to see list of students?\n1 - See\n2 - Skip");
+            System.out.print("Enter: ");
+            Scanner in = new Scanner(System.in);
+            int variantSee = in.nextInt();
+            if (variantSee == 1) {
+                printListOfStudents(studentList);
+                System.out.println("\n");
+            }
 
-           System.out.println(studentList.getStudent(0));
+            System.out.println("1 - Find student by average mark\n2 - Find student by gender\n3 - Find a company by it's activity\n4 - " +
+                    "Find a company by it's date of foundation\n5 - Find a company by the number of staff\n6 - See the list of companies\n7 - Exit");
+            System.out.print("Enter: ");
+            int variantChoise = in.nextInt();
+            switch (variantChoise) {
+                case 1 -> {
+                    System.out.print("Enter average mark: ");
+                    double averageMarkStudent = in.nextDouble();
+                    findStudentByAverageMark(studentList, averageMarkStudent);
+                }
 
-            //studentList.printListOfStudents();
+                case 2 -> {
+                    System.out.println("Enter gender: (1 - male, 2 - female) : ");
+                    int variantGender  = in.nextInt();
+                    System.out.println("\n");
+                    findStudentByGender(studentList, variantGender);
+                }
+
+                case 3 -> {
+
+                }
+            }
         }
         catch (Exception ex) {
             System.out.println("Error :" + ex.getMessage());
--- src/main/java/com/company/Student.java
+++ src/main/java/com/company/Student.java
@@ -1,32 +1,86 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Student {
     private int yearOfBirth;
     private String name;
     private String gender;
     private String faculty;
+    private double averageMark;
     private String favouriteSubject;
     private int course;
     private String colorEyes;
 
-    public Student(int yearOfBirthStudent, String nameStudent, String genderStudent, String facultyStudent, String favouriteSubjectStudent,
+    public Student(int yearOfBirthStudent, String nameStudent, String genderStudent, String facultyStudent, double averageMarkStudent, String favouriteSubjectStudent,
                    int courseStudent, String colorEyesStudent) {
         yearOfBirth = yearOfBirthStudent;
         name = nameStudent;
         gender = genderStudent;
         faculty = facultyStudent;
+        averageMark = averageMarkStudent;
         favouriteSubject = favouriteSubjectStudent;
         course = courseStudent;
         colorEyes = colorEyesStudent;
     }
 
-
     @Override
     public String toString() {
-        return ("Name: " + this.name + "| Gender: " + this.gender + "| Faculty: " + this.faculty +
+        return ("Name: " + this.name + "| Gender: " + this.gender + "| Faculty: " + this.faculty + "| " +"| Average mark: " + this.averageMark +
                 "| Favourite subject: " + this.favouriteSubject + "| Course: " + this.gender + "| Color of eyes: " + this.colorEyes);
     }
 
+    public static void printStudent1(Student student) {
+        System.out.println("Name: " + student.name + "| Gender: " + student.gender + "| Faculty: " + student.faculty + "| " +"| Average mark: " + student.averageMark +
+                "| Favourite subject: " + student.favouriteSubject + "| Course: " + student.gender + "| Color of eyes: " + student.colorEyes);
+    }
+
+    public static void printListOfStudents(List <Student> studentList) {
+        for (int i = 0; i < studentList.size(); ++i) {
+            System.out.println("Name: " + studentList.get(i).name + "| Gender: " + studentList.get(i).gender + "| Faculty: " + studentList.get(i).faculty + "| " +"| Average mark: " + studentList.get(i).averageMark +
+                    "| Favourite subject: " + studentList.get(i).favouriteSubject + "| Course: " + studentList.get(i).gender + "| Color of eyes: " + studentList.get(i).colorEyes);
+        }
+    }
+
+    public static List <Student> readToListStudent(String fileName, String delimeter) throws IOException {
+        File file = new File(fileName);
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+        List<String> stringList = new ArrayList();
+        while ((line = br.readLine()) != null) {
+            stringList.add(line);
+        }
+        br.close();
+        fr.close();
+        List<List<String>> stringListStudents = new ArrayList();
+        for (int i = 0; i < stringList.size(); ++i) {
+            stringListStudents.add(Arrays.asList(stringList.get(i).split(delimeter)));
+        }
+        List <Student> studentListN = new ArrayList<>();
+
+        for (int i = 0; i < stringListStudents.size(); ++i) {
+            Student student = new Student(Integer.parseInt(stringListStudents.get(i).get(0)), stringListStudents.get(i).get(1), stringListStudents.get(i).get(2), stringListStudents.get(i).get(3),
+                    Double.parseDouble(stringListStudents.get(i).get(4)), stringListStudents.get(i).get(5), Integer.parseInt(stringListStudents.get(i).get(6)), stringListStudents.get(i).get(7));
+
+            studentListN.add(student);
+        }
+
+        return studentListN;
+    }
+
+    public static void findByDateOfBirth(List <Student> studentList, int dateOfBirthStudent) {
+        studentList.stream()
+                .filter(p -> p.getYearOfBirth() == dateOfBirthStudent)
+                .forEach(System.out::println);
+    }
+    
     public void setYearOfBirth (int yearOfBirthStudent) {
         this.yearOfBirth = yearOfBirthStudent;
     }
@@ -63,6 +117,15 @@ public class Student {
     }
 
 
+    public void setAverageMark (int averageMarkStudent) {
+        this.averageMark = averageMarkStudent;
+    }
+
+    public double getAverageMark () {
+        return this.averageMark;
+    }
+
+
     public void setFavouriteSubject (String favouriteSubjectStudent) {
         this.favouriteSubject = favouriteSubjectStudent;
     }
@@ -89,4 +152,33 @@ public class Student {
         return this.colorEyes;
     }
 
- }
+    public static void findStudentByGender(List <Student> studentList, int genderStudent) {
+        if (genderStudent == 1) {
+            Optional <Student> java = studentList.stream()
+                    .filter(i -> i.gender.equals("male"))
+                    .findAny();
+            System.out.println(java);;
+        }
+        if (genderStudent == 2) {
+            Optional <Student> java = studentList.stream()
+                    .filter(i -> i.gender.equals("female"))
+                    .findAny();
+
+            System.out.println(java);
+        }
+
+    }
+
+    public static void findStudentByAverageMark(List <Student> studentList, double averageMarkStudent) {
+        Optional <Student> java = studentList.stream()
+                .filter(i -> i.getAverageMark() == averageMarkStudent)
+                .findAny();
+       System.out.println(java);
+    }
+
+    public static void findStudentByFaculty(List <Student> studentList, String facultyStudent) {
+        Stream <Student> java = studentList.stream()
+                .filter(i -> i.getFaculty().equals(facultyStudent))
+
+    }
+}
